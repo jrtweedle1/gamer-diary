3e87e8a1e76067b87b097fc761008ff0b093587e
jrtweedle1 <tweedle@google.com>
2023-11-17 15:48:01 -0600

    created User, Diary, and Section models with references

---
+++ src/main/java/models/Section.java
@@ -0,0 +1,44 @@
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+//@Document annotation to specify the collection
+@Document(collection="sections")
+public class Section {
+
+    //setting up the fields including @Id annotation; marks it as primary key
+    @Id
+    private String id;
+    private String sectionTitle;
+    private String sectionContent;
+
+    //constructor
+    public Diary(String sectionTitle, String sectionContent) {
+        this.sectionTitle = sectionTitle;
+        this.initialProgress = initialProgress;
+    }
+
+    //getters and setters
+    //id
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    //sectionTitle
+    public String getSectionTitle() {
+        return sectionTitle;
+    }
+    public void setSectionTitle(String sectionTitle) {
+        this.sectionTitle = sectionTitle;
+    }
+
+    //sectionContent
+    public String getSectionContent() {
+        return sectionContent
+    }
+    public voice setSectionContent(String sectionContent) {
+        this.sectionContent = sectionContent
+    }
+}
